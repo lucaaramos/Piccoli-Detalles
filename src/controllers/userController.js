0b5b3044e85ff8b0bfa585572d5a043412201ddf
lucaaramos <lucaramos99@gmail.com>
2025-11-03 01:16:13 -0300

    Add crud users and routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,3 +33,42 @@ export const createUsers = async(req, res) => {
     }
 };
 
+export const updateUsers = async(req,res) => {
+    try{
+        const {id} = req.params;
+        const {name, last_name, email, password} = req.body;
+
+        const updateUser = await User.findByIdAndUpdate(
+            id,
+            {name,last_name, email, password}
+        );
+
+        if (!updateUser){
+            return res.status(404).json({error: "User not found"})
+        }
+        res.status(200).json("User updated", updateUser)
+    }
+    catch(error){
+        console.log(error, "Error updating user")
+        res.status(500).json({ erorr: "Server error"})
+    }
+}
+
+export const deleteUser = async(req,res) => {
+    try {
+        const {id} = req.params;
+        
+        const deletedUser = await User.findByIdAndDelete(id);
+        
+        if(!deletedUser){
+            console.log("User not found")
+            return res.status(404).json({error: "User not found"})
+        }
+
+        res.json("User deleted")
+    }catch(error){
+    console.error("Error deleting user")
+    res.status(500).json({error: "Server error"})
+
+    }
+}
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {getUsers, createUsers  } from "../controllers/userController.js";
+import {getUsers, createUsers,updateUsers, deleteUser  } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", createUsers)
-// router.put("/users/:id", updateProduct)
-// router.delete("/users/:id", deleteProduct)
+router.put("/users/:id", updateUsers)
+router.delete("/users/:id", deleteUser)
 
 
 
