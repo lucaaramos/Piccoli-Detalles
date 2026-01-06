63beecb7501d3fd5bd2b3b09c7a2f07159890a48
lucaaramos <lucaramos99@gmail.com>
2026-01-06 16:26:48 -0300

    Add request validation to orders

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -1,16 +1,19 @@
 import express from "express"
 import { createOrder, getAllOrders, getMyOrders, cancelOrder, payOrder, shipOrder } from "../controllers/ordersController.js";
-import { VerifyToken } from "../utils/middlewares.js";
+import { VerifyToken } from "../utils/middlewares/middlewares.js";
 import { isAdmin } from "../utils/isAdmin.js";
+import { validateRequest } from "../utils/middlewares/validateRequest.js";
+import { createOrderSchema } from "../utils/validators/order.schema.js";
+import { mongoIdSchema } from "../utils/validators/common.schema.js";
 
 const router = express.Router();
 
-router.post("/orders", VerifyToken, createOrder);
+router.post("/orders", VerifyToken, validateRequest(createOrderSchema), createOrder);
 router.get("/orders/my/:id", VerifyToken, getMyOrders);
 router.get("/orders", VerifyToken, isAdmin, getAllOrders);
-router.post("/cancel-order/:id", VerifyToken, cancelOrder);
-router.patch("/orders/:id/pay", VerifyToken, payOrder);
-router.patch("/orders/:id/ship", VerifyToken, isAdmin, shipOrder);
+router.post("/cancel-order/:id", VerifyToken, validateRequest(mongoIdSchema, "params"), cancelOrder);
+router.patch("/orders/:id/pay", VerifyToken, validateRequest(mongoIdSchema, "params"), payOrder);
+router.patch("/orders/:id/ship", VerifyToken, isAdmin, validateRequest(mongoIdSchema, "params"), shipOrder);
 
 
 
