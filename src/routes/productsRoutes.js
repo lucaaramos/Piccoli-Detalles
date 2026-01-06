a26183e5754b0179253dbc48d01d355016f86fc8
lucaaramos <lucaramos99@gmail.com>
2026-01-06 16:27:09 -0300

    Add request validations products

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { getProducts, createProduct, deleteProduct, updateProduct } from "../controllers/productController.js";
-import { VerifyToken } from "../utils/middlewares.js";
+import { VerifyToken } from "../utils/middlewares/middlewares.js";
+import { validateRequest } from "../utils/middlewares/validateRequest.js";
+import { productCreateSchema, productUpdateSchema } from "../utils/validators/product.schema.js";
+import { mongoIdSchema } from "../utils/validators/common.schema.js";
+
 const router = express.Router();
 
-router.get("/products", VerifyToken ,getProducts);
-router.post("/products", VerifyToken,createProduct)
-router.put("/products/:id",  VerifyToken,updateProduct)
-router.delete("/products/:id", VerifyToken,deleteProduct)
-
-
+router.get("/products", VerifyToken, getProducts);
+router.post("/products", VerifyToken, validateRequest(productCreateSchema), createProduct);
+router.put("/products/:id", VerifyToken, validateRequest(mongoIdSchema, "params"), validateRequest(productUpdateSchema), updateProduct);
+router.delete("/products/:id", VerifyToken, validateRequest(mongoIdSchema, "params"), deleteProduct);
 
 export default router;
