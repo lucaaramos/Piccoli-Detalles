5da46c5a546510f3161bad7c6819fc6204d41fc2
lucaaramos <lucaramos99@gmail.com>
2026-01-08 19:27:37 -0300

    Add indexes to optimize search, filters and pagination

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
-    stock: {type: Number, required: true, min: 0},
+    stock: { type: Number, required: true, min: 0 },
     imageUrl: { type: String },
     category: { type: String },
   },
@@ -14,14 +14,19 @@ const productSchema = new mongoose.Schema(
     toJSON: {
       virtuals: true,
       transform: (doc, ret) => {
-        ret.id = ret._id.toString(); // agrega "id" legible
-        delete ret._id;              // oculta "_id"
-        delete ret.__v;              // oculta "__v"
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
       },
     },
   }
 );
 
-// Exportar el modelo
+/* Indexes */
+productSchema.index({ name: 1 });          
+productSchema.index({ price: 1 });         
+productSchema.index({ stock: 1 });         
+productSchema.index({ createdAt: -1 });    
+
 const Product = mongoose.model("Product", productSchema);
 export default Product;
