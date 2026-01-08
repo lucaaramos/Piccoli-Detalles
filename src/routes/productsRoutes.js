081b72ec4636429783f273efbccfe7f3e5963de0
lucaaramos <lucaramos99@gmail.com>
2026-01-08 18:58:16 -0300

    Add date, price, order and stock filters

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -4,10 +4,11 @@ import { VerifyToken } from "../utils/middlewares/middlewares.js";
 import { validateRequest } from "../utils/middlewares/validateRequest.js";
 import { productCreateSchema, productUpdateSchema } from "../utils/validators/product.schema.js";
 import { mongoIdSchema } from "../utils/validators/common.schema.js";
+import { productQuerySchema } from "../utils/validators/productSchema.query.js";
 
 const router = express.Router();
 
-router.get("/products", VerifyToken, getProducts);
+router.get("/products", VerifyToken, validateRequest(productQuerySchema, "query"), getProducts);
 router.post("/products", VerifyToken, validateRequest(productCreateSchema), createProduct);
 router.put("/products/:id", VerifyToken, validateRequest(mongoIdSchema, "params"), validateRequest(productUpdateSchema), updateProduct);
 router.delete("/products/:id", VerifyToken, validateRequest(mongoIdSchema, "params"), deleteProduct);
